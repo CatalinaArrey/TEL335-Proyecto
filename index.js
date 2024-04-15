e8495ff8e752a1dc1f0af94ade421b35f28f71c1
Antonia Valenzuela <antonia.valenzuela@sansano.usm.cl>
2024-04-15 19:32:03 -0400

    login

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-const Koa = require('koa');
+const Koa = require("koa");
 // const cors = require('@koa/cors')
-const koaBody = require('koa-bodyparser');
-const Router = require('koa-router')
+const koaBody = require("koa-bodyparser");
+const Router = require("koa-router");
 
 const app = new Koa();
-app.use(koaBody())
+app.use(koaBody());
 // app.use(cors())
 
-var users = []
+var users = [];
 
 const router = new Router();
 
 router.post("/user", async (ctx) => {
-  users.push(ctx.request.body.name)
-  ctx.body = "listo"
-})
+  users.push(ctx.request.body);
+  ctx.body = "listo";
+});
 
 router.get("/user", async (ctx) => {
-  ctx.body = users
-})
+  ctx.body = users;
+});
 
-app.use(router.routes())
+router.post("/login", async (ctx) => {
+  for (let i = 0; i < users.length; i++) {
+    if (
+      users[i].email === ctx.request.body.email &&
+      users[i].password === ctx.request.body.password
+    ) {
+      ctx.body = "beinvenido";
+      break;
+    } else {
+      ctx.body = "fuera";
+    }
+  }
+});
+
+app.use(router.routes());
 
 // app.use(async ctx => {
 //   ctx.body = 'Hello World';
