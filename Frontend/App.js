2b7e6188413da18f6ecf93f6edbd7871e9208792
Catalina Arrey <Catalina.arrey@sansano.usm.cl>
2024-05-27 12:16:05 -0400

    Branch Frontend 2.0

---
+++ Frontend/App.js
@@ -0,0 +1,47 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import RegisterPet from './screens/RegisterPet';
+import Calendario from './screens/Calendars';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Calendar" component={Calendario} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Register Pet" component={RegisterPet} />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={
+              {
+                title: "Mis Mascotas",
+                headerTitleAlign: "center",
+                headerStyle: {
+                  backgroundColor: "#fff",
+                },
+                headerTintColor: "#000",
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+              }}
+            />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
---
+++ Frontend/screens/Login.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import ButtonLogin from '../components/ButtonLogin'
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function Login() {
+    const navigation = useNavigation();
+    const handleRegister =() =>{
+        navigation.navigate("Signup")
+
+    };
+    const handleEnter =() =>{
+        navigation.navigate("Home")
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>¡Hola!</Text>
+            <Text style={styles.subtitle}>Ingresa a tu cuenta</Text>
+            <View style={styles.inputContainer}>
+                <MaterialCommunityIcons name="account" color="#9A9A9A" size={24} style={styles.inputIcon} />
+                <TextInput
+                    placeholder="Username"
+                    style={styles.textInput}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+            <MaterialCommunityIcons name="lock" color="#9A9A9A" size={24} style={styles.inputIcon}/>
+            <TextInput
+                placeholder="Contraseña"
+                style={styles.textInput}
+                secureTextEntry={true}
+            />
+            </View>
+            <TouchableOpacity onPress={handleEnter}>
+            <Text style={styles.links}>¿Olvidaste tu contraseña?</Text>
+            </TouchableOpacity>
+            <ButtonLogin />
+            <TouchableOpacity onPress={handleRegister}>
+            <Text style={styles.footerText}>¿Aún no tienes cuenta? <Text style={styles.links}>Registrate</Text></Text>
+            </TouchableOpacity>
+    
+
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FBFCFC',
+        justifyContent: 'center',
+    },
+    inputContainer: {
+        backgroundColor: '#fff',
+        flexDirection: "row",
+        borderRadius: 30,
+        marginHorizontal: 40,
+        elevation: 10,
+        marginVertical: 10,
+        alignItems: "center",
+    },
+    buttonContainer: {
+        alignItems: "center",
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 60,
+        fontWeight: 'bold',
+        color: '#5DADE2 ',
+    },
+    subtitle: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#2C3E50',
+        marginBottom: 20,
+
+    },
+    textInput: {
+        padding: 10,
+        paddingStart: 5,
+        width: '80%',
+    },
+    inputIcon: {
+        marginLeft: 15,
+    },
+    links: {
+        fontSize: 15,
+        textAlign: "right",
+        color: '#A3E4D7',
+        width: '90%',
+        marginTop: 10,
+        marginRight: 40,
+    },
+    footerText:{
+        color: '#2C3E50',
+        textAlign: 'center',
+        fontSize: 15,
+        marginTop: 60,
+    },
+});
